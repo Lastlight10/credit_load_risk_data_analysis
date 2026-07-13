2f1895113f6991c0f11da9caa733842c5cbefc9a
Lastlight10 <108103857+Lastlight10@users.noreply.github.com>
2026-07-14 01:15:59 +0800

    cleaning income, not finished

---
+++ code.py
@@ -0,0 +1,32 @@
+'''
+edinA = 12000 + 10000 + 7000 + 6500 + 4500 + 6000
+edinQ = 6000 + 5000 + 3500 + 3700 + 3000 + 4000
+edinS = 1800 + 1350 + 875 + 851 + 450 + 560
+
+londonA = 12500 + 11000 + 5500 + 6500 + 4000 + 7000
+londonQ = 7000 + 5000 + 3000 + 3600 + 2500 + 4000
+londonS = 2800 + 1350 + 1110 + 1044 + 700 + 1000
+
+liverpoolA = 11500 + 8000 + 6700 + 5500 + 6000 + 6000
+liverpoolQ = 7400 + 5000 + 4000 + 3000 + 2500 + 2700
+liverpoolS = 2738 + 1750 + 1320 + 960 + 675 +675
+
+manchesterA = 8000 + 7000 + 5000 + 3500 + 5700 +2000
+manchesterS = 1404 + 1269 + 1053 + 675 + 880 + 133
+
+dudleyA = 10000 + 8000 + 6500 + 6500 + 4500 + 5900
+dudleyS = 2280 + 1330 + 1368 + 1786 + 988 + 1710
+
+print(edinS/edinA)
+print(londonS/londonA)
+print(liverpoolS/liverpoolQ)
+print(manchesterS/manchesterA)
+print(dudleyS/dudleyA)
+
+print(manchesterA/(manchesterA+londonA+dudleyA+liverpoolA + edinA))
+
+'''
+x = 7400
+y = 5000
+z= x - y
+print(z/y)
